d582242c4409b5cc59047e01c210ad1f541e85e0
Or Shkefati <or.shkefati@gmail.com>
2026-01-04 17:28:42 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Sudoku PWA - offline cache + update */
-const CACHE = "sudoku-pwa-2.1.8";
+const CACHE = "sudoku-pwa-2.1.9";
 const ASSETS = ["./","./index.html","./manifest.webmanifest","./version.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",(e)=>{
